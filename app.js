aef80e799622a44e20e4e4e84789d493198c39e3
izhanxyno <137381214+izhanxyno@users.noreply.github.com>
2023-06-22 14:32:07 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var http = require('http');
 http.createServer(function (req, res) {  
  res.writeHead(200, {'Content-Type': 'text/html'});
- res.end('Hello!');
+ res.end('Hello World!');
 }).listen(8080, () => {console.log("Server is running at port 8080")});
